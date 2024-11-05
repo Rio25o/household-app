0f3d53f4e402fea2b4df2ec0b9c7bcca33e4ab0c
Rio25o <shutain0621@gmail.com>
2024-11-06 07:34:51 +0900

    記述:Homeの中のMenu、Fromコンポーネントを作成

---
+++ src/components/MonthlySummary.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MonthlySummary = () => {
+  return <div>MonthlySummary</div>;
+};
+
+export default MonthlySummary;
---
+++ src/components/TransactionFrom.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TransactionFrom = () => {
+  return <div>TransactionFrom</div>;
+};
+
+export default TransactionFrom;

92f613d36ed5dff4cf580b8e3224d5e9d88e00ad
Rio25o <shutain0621@gmail.com>
2024-11-11 20:06:42 +0900

    記述:FullCalendarを導入してカレンダーの見た目を作成

--- src/components/Calender.tsx
+++ src/components/Calender.tsx
@@ -1,7 +1,48 @@
+import FullCalendar from "@fullcalendar/react";
 import React from "react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import jaLocale from "@fullcalendar/core/locales/ja";
+import "../calender.css";
+import { EventContentArg } from "@fullcalendar/core";
 
 const Calender = () => {
-  return <div>Calender</div>;
+  const events = [
+    { title: "Meeting", start: "2024-11-11" },
+    {
+      title: "Meeting",
+      start: "2024-11-11",
+      income: 300,
+      expense: 200,
+      balance: 100,
+    },
+  ];
+
+  const renderEventContent = (eventInfo: EventContentArg) => {
+    console.log(eventInfo);
+    return (
+      <div>
+        <div className="money" id="event-income">
+          {eventInfo.event.extendedProps.income}
+        </div>
+        <div className="money" id="event-expense">
+          {eventInfo.event.extendedProps.expense}
+        </div>
+        <div className="money" id="event-balance">
+          {eventInfo.event.extendedProps.balance}
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <FullCalendar
+      locale={jaLocale}
+      plugins={[dayGridPlugin]}
+      initialView="dayGridMonth"
+      events={events}
+      eventContent={renderEventContent}
+    />
+  );
 };
 
 export default Calender;

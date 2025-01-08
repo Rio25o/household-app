a88748578f08b0ffe89a93800c16df7d9fd06eb0
Rio25o <shutain0621@gmail.com>
2025-01-09 03:15:37 +0900

    記述:カレンダーのフォーマットを日本語に修正,（配列:yyyy/MM）

--- src/components/MonthSelector.tsx
+++ src/components/MonthSelector.tsx
@@ -1,20 +1,31 @@
 import { Box, Button } from "@mui/material";
 import React from "react";
-import { LocalizationProvider } from "@mui/x-date-pickers";
-// import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFnsV3";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { ja } from "date-fns/locale";
 
 const MonthSelector = () => {
   return (
-    <LocalizationProvider dateAdapter={AdapterDateFns}>
+    <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={ja}>
       <Box
         sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}
       >
         <Button color={"error"} variant="contained">
           先月
         </Button>
-        <DatePicker sx={{ mx: 2 }} />
+        <DatePicker
+          sx={{ mx: 2 }}
+          views={["year", "month"]}
+          format="yyyy/MM"
+          slotProps={{
+            calendarHeader: { format: "yyyy年 MM月" },
+            toolbar: {
+              toolbarFormat: "yyyy年MM月",
+            },
+          }}
+        />
+
         <Button color={"primary"} variant="contained">
           次月
         </Button>
